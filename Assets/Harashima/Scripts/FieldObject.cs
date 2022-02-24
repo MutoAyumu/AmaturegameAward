170be83ceca2a6ff1ea1bb6a869e58d452f805a4
HarashimaYoshikazu <harashima318@gmail.com>
2022-02-24 18:53:00 +0900

    [add]FieldObjectクラスとインターフェースを追加

---
+++ Assets/Harashima/Scripts/FieldObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+public interface IAction
+{
+    void Action();
+}
