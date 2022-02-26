ec0e3567dbc789a62b1b1e8648b0d83b40f11155
aoyama_saito <saitomarimo1229@gmail.com>
2022-02-27 01:00:16 +0900

    [fix] EnemyMoveを修正

--- Assets/Aoyama/Script/EnemyManager.cs
+++ Assets/Aoyama/Script/EnemyManager.cs
@@ -9,54 +9,6 @@ using UnityEngine;
 public class EnemyManager : MonoBehaviour
 {
     [SerializeField, Tooltip("Enemyをエリアごとに格納する配列")]
-    EnemyGroup[] _enemyGroups;
+    GameObject[] _enemyGroups;
 
-    [SerializeField, Tooltip("現在表示しているEnemyGroup配列のIndex")]
-    int _groupIndex = 0;
-
-    //カプセル化
-    public int GroupIndex { get => _groupIndex;}
-
-    void Start()
-    {
-        _groupIndex = 0;
-
-        _enemyGroups[_groupIndex++].OnSetActive();
-    }
-
-    /// <summary>
-    /// 次のグループを表示する
-    /// </summary>
-    public void NextGroup()
-    {
-        _enemyGroups[_groupIndex].OffSetActive();
-        _enemyGroups[_groupIndex++].OnSetActive();
-    }
-
-    /// <summary>
-    /// 前のグループを表示する
-    /// </summary>
-    public void BackGroup()
-    {
-        _enemyGroups[_groupIndex].OffSetActive();
-        _enemyGroups[_groupIndex--].OnSetActive();
-    }
-
-}
-
-[Serializable]
-class EnemyGroup
-{
-    [SerializeField, Tooltip("EnemyGameObjectのグループ")]
-    GameObject[] _enemyGroup;
-
-    public void OnSetActive()
-    {
-        Array.ForEach(_enemyGroup, go => go.SetActive(true));
-    }
-
-    public void OffSetActive()
-    {
-        Array.ForEach(_enemyGroup, go => go.SetActive(false));
-    }
 }
