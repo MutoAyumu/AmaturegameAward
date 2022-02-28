9eeb5a279a4b9673901ad92f6ce9445e9ffa5545
aoyama_saito <saitomarimo1229@gmail.com>
2022-02-28 14:44:32 +0900

    [Update] PlayerAttackを一旦完成

--- Assets/Aoyama/Script/PlayerAttack.cs
+++ Assets/Aoyama/Script/PlayerAttack.cs
@@ -6,24 +6,17 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    [SerializeField, Tooltip("攻撃時の当たり判定を行うコライダー")]
+    [SerializeField, Tooltip("攻撃の当たり判定を行うコライダー")]
     Collider2D _attackCol;
 
-    Collider2D[] _result = new Collider2D[10];
+    List<Collider2D> _result = new List<Collider2D>(10);
 
-    void Start()
-    {
-        
-    }
-
-    void Update()
-    {
-        
-    }
-
-    void Attack()
+    /// <summary>
+    /// 引数のコライダーの範囲内にいる、IDamageのDamage()を呼ぶ関数
+    /// </summary>
+    public void Attack()
     {
         int count = _attackCol.OverlapCollider(new ContactFilter2D(), _result);
-        //_result.Where(go => go.CompareTag("Enemy")).For
+        _result.ForEach(go => go.GetComponent<IDamage>()?.Damage());
     }
 }
