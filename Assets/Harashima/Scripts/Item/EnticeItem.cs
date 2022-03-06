41a53336dcab90eeee4cc236c1019154bcb1ac3b
Harashima <harashima318@gmail.com>
2022-03-06 16:11:27 +0900

    [update]誘いアイテムの挙動を追加

--- Assets/Harashima/Scripts/Item/EnticeItem.cs
+++ Assets/Harashima/Scripts/Item/EnticeItem.cs
@@ -10,17 +10,24 @@ public class EnticeItem : ItemBase
     [SerializeField, Tooltip("敵を誘うアイテムのプレハブ")]
     GameObject _instansItem;
 
+    GameObject _instans;
+
     public override void Use()
     {
+        //生成する
         if(CharacterManager._instance.Ghost.IsControll)
         {
-            GameObject go = Instantiate(_instansItem, CharacterManager._instance.Ghost.transform.position,Quaternion.identity);
-            //Rigidbody2D rb = go.GetComponent<Rigidbody2D>().AddForce()
+            _instans = Instantiate(_instansItem, CharacterManager._instance.Ghost.transform.position,Quaternion.identity);
         }
         else
         {
-            GameObject go = Instantiate(_instansItem, CharacterManager._instance.Human.transform.position, Quaternion.identity);
+            _instans = Instantiate(_instansItem, CharacterManager._instance.Human.transform.position, Quaternion.identity);
         }
+        //特定の方向に投げる
+        Rigidbody2D rb = _instans.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(0, 1), ForceMode2D.Impulse);
+
+        //数を減らす
         ItemManager.Instance.ItemValueChange(_itemID, -1);
     }
 }
