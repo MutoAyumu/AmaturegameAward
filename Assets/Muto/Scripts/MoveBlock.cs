0ef386323cd33e0555d0df7a54b1a91abd883971
MutoAyumu <mutoayum0610@gmail.com>
2022-03-08 20:52:10 +0900

    [update]MoveBlockスクリプトにギミックを起動させる部分を追加

--- Assets/Muto/Scripts/MoveBlock.cs
+++ Assets/Muto/Scripts/MoveBlock.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class MoveBlock : MonoBehaviour, IHumanGimic
 {
     [SerializeField] Rigidbody2D _rb = default;
+    [SerializeField] GameObject[] _activate = default;
 
     public Rigidbody2D Rb { get => _rb;}
 
     public void Action()
     {
-
+        foreach(var go in _activate)
+        {
+            go.GetComponent<IActivate>()?.Action();
+        }
     }
 }
