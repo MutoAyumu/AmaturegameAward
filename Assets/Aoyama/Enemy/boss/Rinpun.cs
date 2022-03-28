2a9f1fa3a15a2e47c69c72669ec4de6c54c13d67
aoyama_saito <saitomarimo1229@gmail.com>
2022-03-28 20:26:46 +0900

    [Add] prefabsを追加

--- Assets/Aoyama/Enemy/boss/Rinpun.cs
+++ Assets/Aoyama/Enemy/boss/Rinpun.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Rinpun : IAttack
 {
+    [Header("ƒ}ƒYƒ‹")]
     [SerializeField] Transform _rightMuzzle;
     [SerializeField] Transform _leftMuzzle;
     [SerializeField] Transform _upMuzzle;
     [SerializeField] Transform _downMuzzle;
 
+    [Header("’e")]
+    [SerializeField] GameObject _rightBullet;
+    [SerializeField] GameObject _leftBullet;
+    [SerializeField] GameObject _upBullet;
+    [SerializeField] GameObject _downBullet;
+
     public void Attack()
     {
-
+        _rightBullet = GameObject.Instantiate(_rightBullet, _rightMuzzle.position, Quaternion.identity);
+        _leftBullet = GameObject.Instantiate(_leftBullet, _leftMuzzle.position, Quaternion.identity);
+        _upBullet = GameObject.Instantiate(_upBullet, _upMuzzle.position, Quaternion.identity);
+        _rightBullet = GameObject.Instantiate(_rightBullet, _rightMuzzle.position, Quaternion.identity);
     }
 }
