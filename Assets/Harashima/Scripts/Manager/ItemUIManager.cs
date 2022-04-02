0287f5302d03187c826389d996acbb636e10e410
HarashimaYoshikazu <harashima318@gmail.com>
2022-04-02 13:58:56 +0900

    [add]UIManagerを追加

---
+++ Assets/Harashima/Scripts/Manager/ItemUIManager.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// アイテムのUIの更新をするクラス(仮)
+/// </summary>
+public class ItemUIManager : Singleton<ItemUIManager>
+{
+    [SerializeField, Tooltip("アイテムを表示するパネル")]
+    GameObject[] _inventryPanels;
+    public GameObject[] InventryPanels => _inventryPanels;
+
+    [SerializeField, Tooltip("アイテムを表示するイメージ")]
+    GameObject[] _inventryImage;
+
+    [SerializeField, Tooltip("アイテムの個数を表示するUIText")]
+    Text[] _texts;
+    public Text[] Texts => _texts;
+
+    public void ChangeTextValue(int index, int value)
+    {
+        _texts[index].text = value.ToString();
+    }
+
+    public void FirstGet(int index)
+    {
+        _inventryImage[index].SetActive(true);
+        _texts[index].gameObject.SetActive(true);
+    }
+
+    float _timer = 1f;
+    [SerializeField]
+    float _timeInterval = 1f;
+    /// <summary>
+    /// デバッグ用。インプットを受け取る関数（仮）
+    /// </summary>
+    void ItemInput()
+    {
+        _timer += Time.deltaTime;
+        float h = Input.GetAxis("Debug Horizontal");
+        float v = Input.GetAxis("Debug Vertical");
+        if (_timer > _timeInterval)
+        {
+            if (Input.GetKeyDown(KeyCode.Z) || v > 0)
+            {
+                ItemManager.Instance.UseItem(0);
+                _timer = 0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.X) || h > 0)
+            {
+                ItemManager.Instance.UseItem(1);
+                _timer = 0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.C) || v < 0)
+            {
+                ItemManager.Instance.UseItem(2);
+                _timer = 0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.V) || h < 0)
+            {
+                ItemManager.Instance.UseItem(3);
+                _timer = 0f;
+            }
+        }
+    }
+}
