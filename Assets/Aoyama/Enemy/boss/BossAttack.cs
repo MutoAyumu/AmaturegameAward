41568d97486e8a67d52a307b9b09db32e52f09de
aoyama_saito <saitomarimo1229@gmail.com>
2022-04-02 14:55:51 +0900

    [fix] Imageの名前を英語に変更、アニメーションを作成

--- Assets/Aoyama/Enemy/boss/BossAttack.cs
+++ Assets/Aoyama/Enemy/boss/BossAttack.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class BossAttack : MonoBehaviour
 {
-    [SerializeField] IAttack[] _attack;
-
     int _attackIndex = 0;
 
     void Start()
@@ -15,7 +13,6 @@ public class BossAttack : MonoBehaviour
 
     public void RamdomAttack()
     {
-        _attackIndex = Random.Range(0, _attack.Length - 1);
-        _attack[_attackIndex].Attack();
+        
     }
 }
