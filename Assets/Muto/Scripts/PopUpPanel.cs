d4ce2bce130a7083a9b042604caf8a576fdfb91e
MutoAyumu <mutoayum0610@gmail.com>
2022-04-27 20:35:49 +0900

    [update]Font追加・Fieldmanager更新

--- Assets/Muto/Scripts/PopUpPanel.cs
+++ Assets/Muto/Scripts/PopUpPanel.cs
@@ -5,29 +5,40 @@ using UnityEngine.UI;
 
 public class PopUpPanel : MonoBehaviour
 {
-    [SerializeField] Image _panelImage = default;
+    [SerializeField] Image _panel = default;
+    [SerializeField] Image _image = default;
+    [SerializeField]Text _text = default;
+
+    [SerializeField] Sprite _setSprite = default;
+    [SerializeField] string _setText = default;
+
     [SerializeField] GameObject _activeObj = default;
     [SerializeField] string _inputButtonName = "Fire1";
     bool isActive;
 
     private void Start()
     {
-        _panelImage.gameObject.SetActive(false);
+        _panel.gameObject.SetActive(false);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && !isActive)
         {
             isActive = true;
-            _panelImage.gameObject.SetActive(true);
+            _image.sprite = _setSprite;
+            _text.text = _setText;
+            _panel.gameObject.SetActive(true);
+            FieldManager.Instance.Test();
         }
     }
     private void Update()
     {
-        if(Input.GetButtonDown(_inputButtonName))
+        if(Input.GetButtonDown(_inputButtonName) && isActive)
         {
-            _panelImage.gameObject.SetActive(false);
-            _activeObj?.SetActive(true);
+            _panel.gameObject.SetActive(false);
+            _activeObj.SetActive(true);
+            FieldManager.Instance.Test();
+            Destroy(this.gameObject);
         }
     }
 }
