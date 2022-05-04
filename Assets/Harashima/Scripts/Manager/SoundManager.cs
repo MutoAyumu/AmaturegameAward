d2f76811af5355e1bb3d1f082265ad74b55ca0ac
Harashima <harashima318@gmail.com>
2022-05-05 08:31:00 +0900

    [fix]SoundMangerでADXファイルが再生できるように

--- Assets/Harashima/Scripts/Manager/SoundManager.cs
+++ Assets/Harashima/Scripts/Manager/SoundManager.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CriAtomSource))]
 public class SoundManager : Singleton<SoundManager>
 {
     [SerializeField, Tooltip("このコンポーネントのAudioSource")]
     AudioSource _audioSource;
+
+    [SerializeField]
+    CriAtomSource _criAtomSource;
 
     /// <summary>
     /// AudioClipを鳴らす関数
@@ -20,4 +24,30 @@ public class SoundManager : Singleton<SoundManager>
     {
         _audioSource.Play();
     }
+
+    /// <summary>
+    /// ADXファイルを再生する関数
+    /// </summary>
+    /// <param name="cueSheet">鳴らす音の種類をenumから選択</param>
+    /// <param name="cueName">鳴らしたい音のファイル名</param>
+    public void CriAtomPlay(CueSheet cueSheet,string cueName)
+    {
+        if(!_criAtomSource)
+        {
+            _criAtomSource = GetComponent<CriAtomSource>();
+        }
+        _criAtomSource.cueSheet = cueSheet.ToString();
+        _criAtomSource.cueName = cueName;
+        _criAtomSource.Play();
+    }
 }
+
+public enum CueSheet
+{
+    BGM,
+    SE
+}
+
+
+
+
