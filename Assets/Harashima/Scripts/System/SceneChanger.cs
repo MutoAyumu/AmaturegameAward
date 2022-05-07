e9a76438533edae98903c8f548c27fdeadfe9e4b
MutoAyumu <mutoayum0610@gmail.com>
2022-05-07 17:30:28 +0900

    [update]一時ビルド [fix]各シーンの変更 [fix]塔が映るタイミングでフェードなどを追加

--- Assets/Harashima/Scripts/System/SceneChanger.cs
+++ Assets/Harashima/Scripts/System/SceneChanger.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] Image _fadePanel = default;
+    [SerializeField] float _fadeTime = 1;
+    bool IsFade;
 
     public void SceneChange(string name)
     {
-        StartCoroutine(LoadScene(name,2f));
+        //StartCoroutine(LoadScene(name,2f));
+        SceneManager.LoadSceneAsync(name, LoadSceneMode.Single);
     }
 
     private IEnumerator LoadScene(string _name, float _waitTime)
@@ -19,5 +25,32 @@ public class SceneChanger : MonoBehaviour
 
         yield return async;
 
+    }
+
+    public void Fade(GameObject go)
+    {
+        if (!IsFade)
+        {
+            IsFade = true;
+            _fadePanel.DOFade(1, _fadeTime)
+                .OnComplete(() =>
+                {
+                    go.SetActive(true);
+                    Fade(go);
+                });
+        }
+        else
+        {
+            IsFade = false;
+            _fadePanel.DOFade(0, _fadeTime);
+        }
+    }
+    public void FadeSceneChange(string name)
+    {
+        _fadePanel.DOFade(1, _fadeTime)
+            .OnComplete(() =>
+            {
+                SceneChange(name);
+            });
     }
 }
