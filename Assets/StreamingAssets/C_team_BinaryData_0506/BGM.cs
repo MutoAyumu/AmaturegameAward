6e3235c5b831c8c666964e8a883e4d1e21a4d41d
HarashimaYoshikazu <harashima318@gmail.com>
2022-05-09 19:50:38 +0900

    [add]ADXのファイルを入れた

---
+++ Assets/StreamingAssets/C_team_BinaryData_0506/BGM.cs
@@ -0,0 +1,16 @@
+﻿///<summary></summary>
+namespace C_team.BGM {
+	public class BGM {
+		///<summary></summary>
+		public const int BGMField = (0);
+		///<summary></summary>
+		public const int BGMBoss = (1);
+	}
+
+	public enum Cue {
+		///<summary></summary>
+		BGMField = 0,
+		///<summary></summary>
+		BGMBoss = 1,
+	}
+}
