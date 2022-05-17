539b75d4d21edae6ad91be11ec8962e826fb03e0
aoyama_saito <saitomarimo1229@gmail.com>
2022-05-17 21:51:45 +0900

    [Fix] Boss用の弾丸修正

--- Assets/Aoyama/Script/Boss/BossBullet.cs
+++ Assets/Aoyama/Script/Boss/BossBullet.cs
@@ -8,17 +8,14 @@ public class BossBullet : MonoBehaviour
     [SerializeField] Transform _muzzle;
     [Header("‰¹")]
     [SerializeField] CriAtomSource _criAtomSource;
-    [SerializeField] string _cuename = "GaBossRinpun";
+    [SerializeField] string _cuename = "GaBossTama";
 
     public void Instantiate()
-    {
-        Instantiate(_bullet, _muzzle.position, Quaternion.identity);
-    }
-
-    public void BulletSound()
     {
         _criAtomSource.cueSheet = CueSheet.SE.ToString();
         _criAtomSource.cueName = _cuename;
         _criAtomSource.Play();
+        Instantiate(_bullet, _muzzle.position, Quaternion.identity);
     }
+
 }
