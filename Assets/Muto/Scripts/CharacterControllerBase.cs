a2d48da82871aa917a4b9bbf5a79bb202727eb60
MutoAyumu <mutoayum0610@gmail.com>
2022-05-21 20:25:18 +0900

    [update]一旦ゲームシーンをまとめた

--- Assets/Muto/Scripts/CharacterControllerBase.cs
+++ Assets/Muto/Scripts/CharacterControllerBase.cs
@@ -347,7 +347,7 @@ public class CharacterControllerBase : MonoBehaviour
     }
     protected IEnumerator OnDamage(float alpha)
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(2.0f);
 
         _isDamage = false;
         _mainSprite.color = new Color(1, 1, 1, alpha);
